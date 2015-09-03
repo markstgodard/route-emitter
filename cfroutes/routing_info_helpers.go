6a2cc14a0947d54540ad80ce4095d28a9c3e2657
Connor Braa <cbraa@pivotal.io>
2015-09-02 18:03:42 -0700

    wip
    Signed-off-by: Jen Spinney <jen.spinney@hp.com>

--- cfroutes/routing_info_helpers.go
+++ cfroutes/routing_info_helpers.go
@@ -3,6 +3,7 @@ package cfroutes
 import (
 	"encoding/json"
 
+	"github.com/cloudfoundry-incubator/bbs/models"
 	"github.com/cloudfoundry-incubator/receptor"
 )
 
@@ -41,4 +42,36 @@ func CFRoutesFromRoutingInfo(routingInfo receptor.RoutingInfo) (CFRoutes, error)
 	err := json.Unmarshal(*data, &routes)
 
 	return routes, err
+}
+
+// Temporary methods to get around the fact that route emitter and
+// nsync work to remove the receptor are going on in parallel.
+// This should be deleted by whichever track finishes first
+func (c CFRoutes) TemporaryRoutingInfo() *models.Routes {
+	data, _ := json.Marshal(c)
+	routingInfo := json.RawMessage(data)
+	return &models.Routes{
+		CF_ROUTER: &routingInfo,
+	}
+}
+
+func TemporaryCFRoutesFromRoutingInfo(routingInfo *models.Routes) (CFRoutes, error) {
+	if routingInfo == nil {
+		return nil, nil
+	}
+
+	routes := *routingInfo
+	data, found := routes[CF_ROUTER]
+	if !found {
+		return nil, nil
+	}
+
+	if data == nil {
+		return nil, nil
+	}
+
+	cfRoutes := CFRoutes{}
+	err := json.Unmarshal(*data, &cfRoutes)
+
+	return cfRoutes, err
 }
